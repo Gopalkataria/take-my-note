b5da81c92d9cae1119e3e2faa90b860e58d62f98
Glitch (navy-north-nephew) <none>
2020-06-03 15:16:37 +0000

    👜👀 Checkpoint ./src/App.js:3332093/4780 ./src/components/App.js:3332093/64 ./src/index.js:3332093/28

---
+++ src/components/Root.js
@@ -0,0 +1,16 @@
+import React, { Component  } from 'react';
+import './../index.css';
+import "@ionic/react"
+
+
+class App extends Component {
+  render() {
+    return (
+      <ion-app>
+
+      </ion-app>
+    );
+  }
+}
+
+export default App;

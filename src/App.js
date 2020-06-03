681ddb7126418679156d22f4fabdb526d8903f8c
Glitch (navy-north-nephew) <none>
2020-06-03 14:53:36 +0000

    💠⛄️ Checkpoint ./src/App.js:3332093/284

--- src/App.js
+++ src/App.js
@@ -6,36 +6,36 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ion-header color="error">
+        <ion-header mode="ios">
           <ion-toolbar>
-            <ion-title  slot="centre">
-              Clip
+            <ion-title color="primary" size="large" slot="start" >
+              Clippy
 
             </ion-title>
           </ion-toolbar>
-          
-        </ion-header>
-          <p>
-            <ion-card color="primary">
-            
-              <ion-card-content>
 
-              testing
+        </ion-header>
+
+        <ion-card >
+
+          <ion-card-content>
+
+            testing
               </ion-card-content>
-            </ion-card>
+        </ion-card>
 
             Edit <code>src/App.js</code>
-            
-            
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>     
+
+
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+          </a>
       </div>
     );
   }

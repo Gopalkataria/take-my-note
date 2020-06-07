b35c3c46e65334f266c6775d359bf66ab8c11562
Glitch (clippy-art) <none>
2020-06-07 06:13:53 +0000

    🐧🚤 Checkpoint ./src/components/Home.js:3332093/957 ./src/components/Root.jsx:3332093/2

---
+++ src/components/Home.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import './../index.css';
+import "@ionic/react";
+import Header from "./Header";
+// import Edit from "./Edit"
+
+import { items } from "./Root"
+import "@ionic/react-router"
+import { Link } from "react-router-dom";
+
+// let items = [1,1,2,2,3,3]
+
+
+class Home extends Component {
+    render() {
+
+        return (
+
+            <>
+
+
+                <Header atHome={true} />
+
+
+                <ion-content fullscreen={true} scrollY={true} class="has-footer" overflow-scroll="false">
+                    <ion-list>
+
+
+                        {
+                            items.map((el) => {
+
+                                return (
+                                    <ion-card>
+
+
+                                        <ion-card-content>
+                                            <Link to="/edit" >
+
+                                                <ion-card-title>
+                                                    {el}
+                                                </ion-card-title>
+
+                                            </Link>
+
+                                        card no. = {el}
+
+                                        </ion-card-content>
+                                    </ion-card>
+
+                                )
+
+
+                            })
+
+                        }
+
+                    </ion-list>
+
+
+
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+
+                </ion-content>
+
+            </>
+
+
+        )
+    }
+}
+
+export default Home; 

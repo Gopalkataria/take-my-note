b8eb3101ac08d678dba6d9d6f7e4b8492c22bdca
Glitch (clippy-art) <none>
2020-06-19 15:11:28 +0530

    merged multiple files into one to experiment with loading times

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,11 +7,87 @@ import {
 	IonHeader,
 	IonToolbar,
 	IonTitle,
+	IonItem,
+	IonTextarea,
+	IonText,
 	IonButton,
+	IonLabel,
 } from "@ionic/react";
-import { pencilSharp, saveOutline } from "ionicons/icons";
-import PersistentTextBox from "../components/PersistentTextBox";
-import Greeting from "../components/Greeting"
+import { saveOutline } from "ionicons/icons";
+
+
+
+class PersistentTextBox extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { text: "" };
+	}
+
+	updateLocalStorage = (evt) => {
+		evt.preventDefault();
+		this.setState({
+			text: evt.detail.value,
+		});
+		localStorage.setItem("text", this.state.text);
+	};
+
+	componentDidMount() {
+		console.log("updating");
+
+		window.addEventListener( "load" , () => {
+			console.log("loaded")
+
+			document.getElementById("textarea").setAttribute("auto-grow" , "true")
+		})
+		this.setState({
+			text: localStorage.getItem("text"),
+		});
+	}
+
+	render() {
+		return (
+			<IonItem>
+				<IonLabel position="floating">My Note :</IonLabel>
+				<IonTextarea
+					id="textarea"
+					className="ion-text ion-padding"
+					rows={20}
+					enterkeyhint="enter"
+					inputMode="text"
+					debounce="100"
+					maxlength={5000}
+					spellCheck={true}
+					onIonChange={this.updateLocalStorage}
+					placeholder="Enter your text here... The place holder will resize automatically if needed "
+					value={this.state.text}
+					mode="ios"
+				></IonTextarea>
+			</IonItem>
+		);
+	}
+}
+
+
+
+class Greeting extends Component {
+	render() {
+		if (localStorage.getItem("text")) {
+			return <div></div>;
+		} else {
+			return (
+				<IonItem>
+					<IonText class="ion-margin ion-text-center">
+						Type in anything here and it will be saved on your device for
+						eternity, you can access it anytime anywhere not only on this
+						device. And you can even use it offline !! Make sure you don't clear
+						cache or reset any device browser settings for this browser.
+					</IonText>
+				</IonItem>
+			);
+		}
+	}
+}
+
 
 
 class Home extends Component {
@@ -20,8 +96,7 @@ class Home extends Component {
 			<IonPage>
 				<IonHeader>
 					<IonToolbar>
-						<IonIcon class="ion-padding" icon={pencilSharp} slot="start" />
-						<IonTitle size="large" slot="primary" class='ion-text-align-center'>
+						<IonTitle size="large" color="primary"  slot="start" class='ion-text-align-center'>
 
 							Note
 						</IonTitle>
